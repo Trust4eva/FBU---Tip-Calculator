2989593447a4344ac0c50e4ae07b233bde0cd18a
Trustin Harris <trust4eva@fb.com>
2018-06-26 13:13:07 -0700

    Initial Commit

---
+++ Tip Calc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calc
+//
+//  Created by Trustin Harris on 6/26/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tip Calc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tip Calc
+//
+//  Created by Trustin Harris on 6/26/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
