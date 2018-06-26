a4f8619044f7256cb366e79f446c748e26859fed
Trustin Harris <trust4eva@fb.com>
2018-06-26 16:43:05 -0700

    commit

--- Tip Calc/ViewController.m
+++ Tip Calc/ViewController.m
@@ -9,14 +9,60 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *BillField;
+@property (weak, nonatomic) IBOutlet UILabel *TipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *TotalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *TipSegmentedControl;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)Tap:(id)sender {
+    [self.view endEditing:YES]; // Gets rid of keyboard
+}
+    
+- (IBAction)EditText:(id)sender {
+    
+    NSArray *percents = @[@(0.1), @(0.15), @(0.2)];
+    
+    double tipPercents = [percents[self.TipSegmentedControl.selectedSegmentIndex] doubleValue];
+    
+    double bill = [self.BillField.text doubleValue];
+    double tip = tipPercents * bill;
+    double total = bill + tip;
+    
+    self.TipLabel.text = [NSString stringWithFormat: @"$%.2f", tip];
+    self.TotalLabel.text = [NSString stringWithFormat: @"$%.2f", total];
+}
+
+- (IBAction)EditingBegan:(id)sender {
+    self.BillField.text = @"";
+    
+}
+- (IBAction)EditingEnd:(id)sender {
+   
+}
+- (IBAction)Slider:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    NSString *number;
+    number = [NSString stringWithFormat:@"$%.2f", slider.value];
+    
+     double bill = [self.BillField.text doubleValue];
+    double tip = [[number substringFromIndex:1] doubleValue];
+    double total = bill + tip;
+    self.TotalLabel.text = [NSString stringWithFormat: @"$%.2f", total];
+    
+    
+    
+    self.TipLabel.text = number;
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.BillField.text = [NSString stringWithFormat: @"0.00"];
 }
 
 
